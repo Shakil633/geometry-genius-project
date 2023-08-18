22194017c4661114273807087ad7fa1825eeed70
unknown <saklislam633@gmail.com>
2023-08-18 17:35:45 +0600

    added calculation part

--- scripts/area-calulate.js
+++ scripts/area-calulate.js
@@ -1,54 +1,83 @@
+// get triangle base value
+
 function calculateFunctionArea() {
-  // get triangle base value
   const baseField = document.getElementById("triangle-base");
   const baseValueText = baseField.value;
   const base = parseFloat(baseValueText);
-  console.log(base);
+  baseField.value = "";
+  
 
   //get triangle height value
 
   const heightField = document.getElementById("triangle-height");
   const heightValueText = heightField.value;
   const height = parseFloat(heightValueText);
-  console.log(height);
+  heightField.value = "";
+
+  // Triangle: Validate input and length (aikhane moloto amra dekhaichi jeh jodi number nah dei tahole please insert a number dekhaibe)
+
+  if (isNaN(base) || isNaN(height)) {
+    alert("Please insert a number");
+    return;
+  }
 
   const area = 0.5 * base * height;
   console.log(area);
 
   document.getElementById("area").innerText = area;
+
+   // add to calculation
+   calculationEntry("Triangle: ", area);
 }
 
+// get rectangle with
 function calculateRectangle() {
-  // get rectangle with
   const widthField = document.getElementById("rectangle-with");
   const widthValueText = widthField.value;
   const width = parseFloat(widthValueText);
-  console.log(width);
+  widthField.value = "";
 
   //get triangle height value
 
   const lengthField = document.getElementById("rectangle-length");
   const lengthValueText = lengthField.value;
   const length = parseFloat(lengthValueText);
-  console.log(length);
+  lengthField.value = "";
 
+  // Rectangle: Validate input and length (aikhane moloto amra dekhaichi jeh jodi number nah dei tahole please insert a number dekhaibe)
+
+  if (isNaN(width) || isNaN(length)) {
+    alert("Please insert a number");
+    return;
+  }
   //calculate area
   const area = width * length;
-  console.log(area);
-
   // show rectangle area
   document.getElementById("rectangle-area").innerText = area;
+
+  // add to calculation
+  calculationEntry("Rectangle: ", area);
 }
 
 // reusable function---> reduce duplicate code
+
+// Parallelogram area
 function calculateParallelogram() {
   const base = getInputValue("parallelogram-base");
-
   const height = getInputValue("parallelogram-height");
+
+  // validate
+  if (isNaN(base) || isNaN(height)) {
+    alert("Please insert a number");
+    return;
+  }
 
   const area = base * height;
 
   setElementInnerText("parallelogram-area", area);
+
+  // add to calculation
+  calculationEntry("parallelogram: ", area);
 }
 
 // Rhombus Area
@@ -57,9 +86,18 @@ function calculateRhombusArea() {
   const rhombusD1 = getInputValue("rhombus-base");
   const rhombusD2 = getInputValue("rhombus-height");
 
-  const area = 0.5 * rhombusD1 * rhombusD2;
+  // validate
+  if (isNaN(rhombusD1) || isNaN(rhombusD2)) {
+    alert("Please insert a number");
+    return;
+  }
 
+  const area = 0.5 * rhombusD1 * rhombusD2;
   setElementInnerText("rhombus-area", area);
+
+   // add to calculation
+   calculationEntry("Rhombus: ", area);
+
 }
 
 // Pentagon Area
@@ -68,17 +106,36 @@ function calculatePentagonArea() {
   const pentagonP = getInputValue("pentagon-p");
   const pentagonBase = getInputValue("pentagon-base");
 
+  // validate
+  if (isNaN(pentagonP) || isNaN(pentagonBase)) {
+    alert("Please insert a number");
+    return;
+  }
+
   const area = 0.5 * pentagonP * pentagonBase;
   setElementInnerText("pentagon-area", area);
+
+   // add to calculation
+   calculationEntry("Pentagon: ", area);
 }
 
 //Ellipse
 function calculateEllipseArea() {
   const ellipseA = getInputValue("ellipse-a");
   const ellipseBase = getInputValue("ellipse-base");
-  const area = 3.1416 * ellipseA * ellipseBase;
 
-  setElementInnerText("ellipse-area", area);
+  // validate
+  if (isNaN(ellipseA) || isNaN(ellipseBase)) {
+    alert("Please insert a number");
+    return;
+  }
+
+  const area = 3.14 * ellipseA * ellipseBase;
+  const areaTwoDecimal = area.toFixed(2);
+  setElementInnerText("ellipse-area", areaTwoDecimal);
+
+   // add to calculation
+   calculationEntry("Ellipse: ", areaTwoDecimal);
 }
 
 //reusable get input value filed in number function
@@ -87,6 +144,7 @@ function getInputValue(id) {
   const inputField = document.getElementById(id);
   const inputValueText = inputField.value;
   const value = parseFloat(inputValueText);
+  inputField.value = "";
   return value;
 }
 
@@ -95,3 +153,34 @@ function setElementInnerText(elementId, area) {
   const element = document.getElementById(elementId);
   element.innerText = area;
 }
+
+//add to calculation entry
+/*
+1. get the element where you want to add the dynamic HTML
+2.create and element you want to add
+3.if needed add some class
+4.set inner HTML. it could be dynamic Template string
+5.append the created element as a child of the parent
+*/
+
+function calculationEntry(areaType, area) {
+  console.log("adding song");
+  const calculationEntry = document.getElementById("calculation-entry");
+
+  const count=calculationEntry.childElementCount
+
+  // akta p tag add korte pari
+  const p = document.createElement("p");
+  p.classList.add('my-4')
+  p.innerHTML = `${count + 1}. ${areaType} ${area} cm<sup>2</sup>`
+
+  // kono likha akjaigate add kora jonno .appendChild korte hobe.
+  calculationEntry.appendChild(p);
+}
+
+//data validation
+/*
+1.set the proper type of the input field. (number,data,email)
+2.check type using typeof
+3.NaN means: Not a number. isNaN is checking whether the input is not a number or not
+*/
